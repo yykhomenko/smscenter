f463d24d93150a022dba7640bbdfe3b122e6ca9b
yykhomenko <yykhomenko@gmail.com>
2022-10-18 15:28:19 +0200

    clean

--- cmd/smscenter/main.go
+++ cmd/smscenter/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	s := server.NewServer("localhost:3736")
+	s := server.NewServer("localhost:2775")
 	defer s.Close()
 	log.Println("smscenter listen to:", s.Addr())
 	fmt.Scanln()
